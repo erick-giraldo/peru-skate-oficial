9abdb6780a95e46c1604bc87de175141ba9df5aa
Erick <erick.giraldo@banexcoin.com>
2024-07-22 17:26:15 -0500

    revert

---
+++ src/pages/Features/featuresData.tsx
@@ -0,0 +1,96 @@
+import { Feature } from "@/components/types/feature";
+
+const featuresData: Feature[] = [
+  {
+    id: 1,
+    image: '/images/feature/feat_01.webp',
+    title: "Crafted for Startups",
+    paragraph:
+      "Tuae nam ex similique incidunt expedita exerci tationem laudantium. Repellendus quisquam numquam perferendis earum sapiente non tempore? Fugit repellat ut maiores.",
+  },
+  {
+    id: 1,
+    image: '/images/feature/feat_02.webp',
+    title: "High-quality Design",
+    paragraph:
+      "Tuae nam ex similique incidunt expedita exerci tationem laudantium. Repellendus quisquam numquam perferendis earum sapiente non tempore? Fugit repellat ut maiores.",
+  },
+  {
+    id: 1,
+    image: '/images/feature/feat_03.webp',
+    title: "Next.js 13 (Latest)",
+    paragraph:
+      "Tuae nam ex similique incidunt expedita exerci tationem laudantium. Repellendus quisquam numquam perferendis earum sapiente non tempore? Fugit repellat ut maiores.",
+  },
+  {
+    id: 1,
+    image: '/images/feature/feat_04.webp',
+    title: "Tailwind CSS",
+    paragraph:
+      "Tuae nam ex similique incidunt expedita exerci tationem laudantium. Repellendus quisquam numquam perferendis earum sapiente non tempore? Fugit repellat ut maiores.",
+  },
+  {
+    id: 1,
+    image: '/images/feature/feat_05.webp',
+    title: "Fully Customizable",
+    paragraph:
+      "Tuae nam ex similique incidunt expedita exerci tationem laudantium. Repellendus quisquam numquam perferendis earum sapiente non tempore? Fugit repellat ut maiores.",
+  },
+  {
+    id: 1,
+    image: '/images/feature/feat_06.webp',
+    title: "Free and Open-Source",
+    paragraph:
+      "Tuae nam ex similique incidunt expedita exerci tationem laudantium. Repellendus quisquam numquam perferendis earum sapiente non tempore? Fugit repellat ut maiores.",
+  },
+  {
+    id: 1,
+    image: '/images/feature/feat_07.webp',
+    title: "Free and Open-Source",
+    paragraph:
+      "Tuae nam ex similique incidunt expedita exerci tationem laudantium. Repellendus quisquam numquam perferendis earum sapiente non tempore? Fugit repellat ut maiores.",
+  },
+  {
+    id: 1,
+    image: '/images/feature/feat_08.webp',
+    title: "Free and Open-Source",
+    paragraph:
+      "Tuae nam ex similique incidunt expedita exerci tationem laudantium. Repellendus quisquam numquam perferendis earum sapiente non tempore? Fugit repellat ut maiores.",
+  },
+  {
+    id: 1,
+    image: '/images/feature/feat_09.webp',
+    title: "Free and Open-Source",
+    paragraph:
+      "Tuae nam ex similique incidunt expedita exerci tationem laudantium. Repellendus quisquam numquam perferendis earum sapiente non tempore? Fugit repellat ut maiores.",
+  },
+  {
+    id: 1,
+    image: '/images/feature/feat_10.webp',
+    title: "Free and Open-Source",
+    paragraph:
+      "Tuae nam ex similique incidunt expedita exerci tationem laudantium. Repellendus quisquam numquam perferendis earum sapiente non tempore? Fugit repellat ut maiores.",
+  },
+  {
+    id: 1,
+    image: '/images/feature/feat_11.webp',
+    title: "Free and Open-Source",
+    paragraph:
+      "Tuae nam ex similique incidunt expedita exerci tationem laudantium. Repellendus quisquam numquam perferendis earum sapiente non tempore? Fugit repellat ut maiores.",
+  },
+  {
+    id: 1,
+    image: '/images/feature/feat_12.webp',
+    title: "Free and Open-Source",
+    paragraph:
+      "Tuae nam ex similique incidunt expedita exerci tationem laudantium. Repellendus quisquam numquam perferendis earum sapiente non tempore? Fugit repellat ut maiores.",
+  },
+  {
+    id: 1,
+    image: '/images/feature/feat_13.webp',
+    title: "Free and Open-Source",
+    paragraph:
+      "Tuae nam ex similique incidunt expedita exerci tationem laudantium. Repellendus quisquam numquam perferendis earum sapiente non tempore? Fugit repellat ut maiores.",
+  },
+];
+export default featuresData;
