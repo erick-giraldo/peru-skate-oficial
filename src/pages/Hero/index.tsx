6d9b0b834b7b507b7f48b969fcd2b2bddf6d20f7
erick-giraldo <egiraldom@outlook.com>
2024-10-14 10:26:04 -0500

    opacity video

--- src/pages/Hero/index.tsx
+++ src/pages/Hero/index.tsx
@@ -6,7 +6,10 @@ interface HeroProps {
 
 export default function Hero({ src }: HeroProps) {
   const handleClick = () => {
-    window.open("https://api.whatsapp.com/send/?phone=51945970045&text=🔥Deseo%20reservar%20una%20clase%20de%20prueba", "_blank"); // Cambia la ruta a la que deseas redirigir
+    window.open(
+      "https://api.whatsapp.com/send/?phone=51945970045&text=🔥Deseo%20reservar%20una%20clase%20de%20prueba",
+      "_blank"
+    );
   };
 
   return (
@@ -15,7 +18,7 @@ export default function Hero({ src }: HeroProps) {
         id="home"
         className="relative z-10 overflow-hidden bg-white h-70vh flex items-center"
       >
-        <div className="absolute inset-0 z-0">
+        <div className="absolute z-0 inset-0">
           <video
             className="w-full h-full object-cover"
             crossOrigin="anonymous"
@@ -27,12 +30,13 @@ export default function Hero({ src }: HeroProps) {
             autoPlay
             src={src}
           ></video>
+          <div className="absolute inset-0 bg-black opacity-50"></div>
         </div>
         <div className="relative z-10 py-10 px-10">
           <div className="flex flex-col items-start sm:pr-[50%]">
             <h1 className="font-dreadful mb-5  leading-tight text-white text-[60px] sm:text-[60px] sm:leading-tight lg:text-[80px] xl:text-[111px] md:leading-tight">
-            ¡Bienvenido, Rider!
-            {/* Clases de skateboarding en lima, perú */}
+              ¡Bienvenido, Rider!
+              {/* Clases de skateboarding en lima, perú */}
             </h1>
             <p className="mb-2 text-white text-body-color sm:text-2xl md:text-xl">
               ¡Desarrolla tus habilidades y destrezas al máximo! Ofrecemos
@@ -46,7 +50,7 @@ export default function Hero({ src }: HeroProps) {
               <Button
                 onClick={handleClick}
                 variant="default"
-                className="md:h-14 lg:px-8 h-12 md:px-4  w-60  md:w-80 rounded-xl bg-[#2f76b4] text-xs lg:text-base font-semibold text-white duration-300 ease-in-out hover:bg-[#2f76b4]/80" 
+                className="md:h-14 lg:px-8 h-12 md:px-4  w-60  md:w-80 rounded-xl bg-[#2f76b4] text-xs lg:text-base font-semibold text-white duration-300 ease-in-out hover:bg-[#2f76b4]/80"
               >
                 🔥 Reserva tu clase de prueba gratis
               </Button>
