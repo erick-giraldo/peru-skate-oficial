4eadf967a21b8cf61e4ffc41dfd577cd02e8838a
erick-giraldo <egiraldom@outlook.com>
2024-10-18 22:38:24 -0500

    cambio3

--- src/pages/Pricing/index.tsx
+++ src/pages/Pricing/index.tsx
@@ -69,19 +69,20 @@ const Pricing = () => {
                 : "Paquete por 4 clases: Ideal para empezar."
             }
           >
-            <OfferList text="1 sesión semanal " status="active" />
+            <OfferList text="1 sesión semanal" status="active" />
             <OfferList text="Clases básicas" status="active" />
-            <OfferList text="Implementos de profesionles" status="active" />
+            <OfferList text="Implementos profesionales" status="active" />
             <OfferList text="Confianza y coordinación" status="active" />
             <OfferList text="Superación y autoestima" status="active" />
             <OfferList text="Cultura skate" status="active" />
-            <OfferList text="Desarrollo de vinculos sociales" status="active" />
+            <OfferList text="Desarrollo de vínculos sociales" status="active" />
             <OfferList
-              text="Durante el  primer mes se le presta todos los implementos"
+              text="Durante el primer mes se le prestan todos los implementos"
               status="active"
               classText="text-navBurger"
             />
           </PricingBox>
+
           <PricingBox
             packageName="Intermedio"
             price={isMonthly ? "70" : "400"}
@@ -94,17 +95,18 @@ const Pricing = () => {
           >
             <OfferList text="2 sesiones semanales" status="active" />
             <OfferList text="Técnicas avanzadas" status="active" />
-            <OfferList text="Implementos de profesionles" status="active" />
+            <OfferList text="Implementos profesionales" status="active" />
             <OfferList text="Ejercicio de coordinación motriz" status="active" />
             <OfferList text="Creatividad y perseverancia" status="active" />
             <OfferList text="Eventos locales" status="active" />
             <OfferList text="Clases en CARPS" status="active" />
             <OfferList
-              text="Durante el  primer mes se le presta todos los implementos"
+              text="Durante el primer mes se le prestan todos los implementos"
               status="active"
               classText="text-navBurger"
             />
           </PricingBox>
+
           <PricingBox
             packageName="Pro"
             price={isMonthly ? "70" : "540"}
@@ -115,30 +117,32 @@ const Pricing = () => {
                 : "Paquete por 12 clases: Entrenamiento pro."
             }
           >
-            <OfferList text="3 sesiónes semanales" status="active" />
+            <OfferList text="3 sesiones semanales" status="active" />
             <OfferList text="Plan personalizado y grupal" status="active" />
-            <OfferList text="Implementos de profesionles" status="active" />
-            <OfferList text="Prep. física y elasticidad motriz" status="active" />
-            <OfferList text="Técnicas avanzadas y de explosión muscular" status="active" />
+            <OfferList text="Implementos profesionales" status="active" />
+            <OfferList text="Preparación física y elasticidad motriz" status="active" />
+            <OfferList text="Técnicas avanzadas y explosión muscular" status="active" />
             <OfferList text="Preparación competitiva" status="active" />
             <OfferList text="Competencias nacionales" status="active" />
             <OfferList
-              text="Durante el  primer mes se le presta todos los implementos"
+              text="Durante el primer mes se le prestan todos los implementos"
               status="active"
               classText="text-navBurger"
             />
           </PricingBox>
+
           <div id="products"></div>
         </div>
       </div>
-      <div className="mt-16 w-full bg-gradientCool dark:bg-gray-800 p-8 shadow-lg">
+
+      <div className="mt-16 w-full bg-gradientDark dark:bg-gray-800 p-8 shadow-lg">
         <div className="container">
           <h3 className="text-3xl font-bold mb-4 text-center text-white font-dreadful md:text-[50px]">
             Implementos a la Venta
           </h3>
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
             <div className="text-white p-4 rounded-md">
-              <h4 className="font-bold mb-2  md:text-[25px]">Kit Completo de Protecciones</h4>
+              <h4 className="font-bold mb-2 md:text-[25px]">Kit Completo de Protecciones</h4>
               <p>Rodilleras, coderas y muñequeras</p>
               <p className="font-semibold mt-2">Precio: S/350</p>
             </div>
@@ -149,7 +153,7 @@ const Pricing = () => {
             </div>
             <div className="text-white p-4 rounded-md">
               <h4 className="font-bold mb-2 md:text-[25px]">Skate Pro Completo</h4>
-              <p>Equipo profesional para tu entrenamiento</p>
+              <p>Equipo profesional para tu entrenamiento, piezas originales.</p>
               <p className="font-semibold mt-2">Precio: S/650</p>
             </div>
           </div>
