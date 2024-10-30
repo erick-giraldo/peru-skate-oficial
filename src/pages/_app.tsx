2dc975db93dc137e5744b7790d1dda6ee533c0d9
erick-giraldo <egiraldom@outlook.com>
2024-10-30 12:11:00 -0500

    fix AOS height

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,10 +7,14 @@ import "aos/dist/aos.css";
 export default function App({ Component, pageProps }: AppProps) {
   useEffect(() => {
     AOS.init({
-      duration: 2000, // Duración de la animación en milisegundos
-      once: true, // Ejecuta la animación solo una vez al hacer scroll
+      duration: 2000,
+      once: true,
     });
   }, []);
 
-  return <Component {...pageProps} />;
+  return (
+    <div style={{ overflowX: "hidden" }}>
+      <Component {...pageProps} />
+    </div>
+  );
 }
